dd7d187699e545325bbfda65120ac3e704997ad6
thonguyen100 <haithonguyen0305@gmail.com>
2025-09-18 08:54:57 +0700

    New UI Test 1

--- src/elements/Home.jsx
+++ src/elements/Home.jsx
@@ -67,7 +67,7 @@ function Home() {
     <div className="brutal-container">
       <div className="brutal-content">
         <div className="brutal-header">
-          <h1 className="brutal-title brutal-glitch" data-text="Students Database">
+          <h1 className="brutal-title brutal-glitch" data-text="Wiz Tech Finance">
             Students Database
           </h1>
           <Link className="brutal-btn brutal-btn-success" to="/create">
